f39c06938aca83bcf6c872700f784d2d8bca557c
unknown <leo_medina14@hotmail.com>
2020-02-24 17:05:50 -0500

    configuracion de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCoqkIiF47W9VcQDtBgn4gt-R3xYL-po9I',
+    authDomain: 'equipo-futbol.firebaseapp.com',
+    databaseURL: 'https://equipo-futbol.firebaseio.com',
+    projectId: 'equipo-futbol',
+    storageBucket: 'equipo-futbol.appspot.com',
+    messagingSenderId: '807780101932',
+    appId: '1:807780101932:web:94802c21847a32ed440689',
+    measurementId: 'G-L2KRHYVLG6'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCoqkIiF47W9VcQDtBgn4gt-R3xYL-po9I',
+    authDomain: 'equipo-futbol.firebaseapp.com',
+    databaseURL: 'https://equipo-futbol.firebaseio.com',
+    projectId: 'equipo-futbol',
+    storageBucket: 'equipo-futbol.appspot.com',
+    messagingSenderId: '807780101932',
+    appId: '1:807780101932:web:94802c21847a32ed440689',
+    measurementId: 'G-L2KRHYVLG6'
+  }
 };
 
 /*
